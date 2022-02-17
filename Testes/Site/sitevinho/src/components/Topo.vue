50ca7fa87ff5fc1dede5b7af803024d08d577c89
VICTOR AUGUSTO DE ALMEIDA SILVA <victor.aasilva2@senacsp.edu.br>
2022-02-17 16:26:50 -0300

    Adicionando o header do site

---
+++ Testes/Site/sitevinho/src/components/Topo.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <div class="container">
+  <div class="row">
+    <div id="topo" class="col navbar fixed-top ">
+      <a class="navbar-brand" href="#"><img id="logo" src="./imagens/logo.png" alt="Ícone do site"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
