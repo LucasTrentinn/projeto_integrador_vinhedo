d78e35185bfdd279767fc9d2ff180e47e85f6cae
Lucas Trentin <lucas.mtrentin@hotmail.com>
2022-02-18 16:22:01 -0300

    Adicionando arquivos do rodapé

--- Site/Site_Vinho/src/App.vue
+++ Site/Site_Vinho/src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  
+  <rodape />
 </template>
 
 <script>
+import Rodape from '../../../../Aulas/SENAC_UC3/aula_17/tarefas/src/components/Rodape.vue'
 export default {
+  components: { Rodape },
 
 }
 </script>
