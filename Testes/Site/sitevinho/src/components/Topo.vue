4d7edee8a3ea29979185298aaf3b285515a9f167
VICTOR AUGUSTO DE ALMEIDA SILVA <victor.aasilva2@senacsp.edu.br>
2022-02-18 13:42:47 -0300

    atualização do site

--- Testes/Site/sitevinho/src/components/Topo.vue
+++ Testes/Site/sitevinho/src/components/Topo.vue
@@ -3,9 +3,12 @@
     <div class="container-fluid">
       <div class="row topo">
         <div class="col-1">
-          <a class="navbar-brand" href="#"><img id="logo" src="./imagens/logo.png" alt="Ícone do site"></a>
+          <a class="navbar-brand" href="https://betteranime.net/" target="_blank"><img id="logo" src="./imagens/logo.png" alt="Ícone do site"></a>
         </div>
-        <div class="col-5">
+        <div id="menu" class="col-5">
+          <a href="#">Vinhos</a>
+          <a href="#">Sobre</a>
+          <a href="#">contato</a>
         </div>
         <div class="col-5 campoPesquisa">
           <input type="text" placeholder="Busque por vinhos e outros usuários..." autocomplete="off" id="campoPesquisa" size="70" onfocus="this.value='';">
@@ -25,5 +28,36 @@ export default {
 </script>
 
 <style>
+
+.topo {
+    background-color: #180566;
+    height: 100px;
+    color: white;
+}
+
+#logo{
+    max-width: 100px;
+    max-height: 100px;
+    height: auto;
+    width: auto;
+}
+
+.campoPesquisa {
+    display: flex;
+    justify-content: right;
+    align-items: center;
+}
+
+#campoPesquisa {
+    border-radius: 20px;
+    height: 45px;
+}
+
+.topo4 {
+    display: flex;
+    align-items: center;
+}
+
+
 
 </style>
