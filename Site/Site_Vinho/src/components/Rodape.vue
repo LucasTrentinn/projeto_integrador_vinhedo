e700c27f3c6c76b622499339a513134f4d742da6
Lucas Trentin <lucas.mtrentin@hotmail.com>
2022-02-18 17:14:42 -0300

    Criação da estrutura pra montagem do rodapé

--- Site/Site_Vinho/src/components/Rodape.vue
+++ Site/Site_Vinho/src/components/Rodape.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="container">
-      
-  </div>
+    <div class="container-fluid rodape fixed-bottom">
+          <div class="row conteudo">
+            <div class="col-4">
+                <h1>teste</h1>
+            </div>
+            <div class="col-4">
+                <h1>teste</h1>
+            </div>
+            <div class="col-4">
+                <h1>teste</h1>
+            </div>
+        </div>
+        <div class="row copyright">
+            <p>&copy; 2021-2022 projeto por Lucas, Victor e Ana</p>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,5 +24,18 @@ export default {
 </script>
 
 <style>
-
+.rodape {
+    background-color: #180566;
+    height: 250px;
+}
+.conteudo {
+    height: 90%;
+}
+.copyright {
+    height: 10px;
+    font: 15px Georgia normal;
+    color: white;
+    font-display: flex;
+    justify-content: center;
+}
 </style>
