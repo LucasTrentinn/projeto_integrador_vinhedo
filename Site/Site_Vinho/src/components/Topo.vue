3f36b0e41aaaf5e2a4d150902f6599c65d8a9020
Lucas Trentin <lucas.mtrentin@hotmail.com>
2022-02-21 17:17:53 -0300

    Upload do Topo.vue quase completo

--- Site/Site_Vinho/src/components/Topo.vue
+++ Site/Site_Vinho/src/components/Topo.vue
@@ -1,40 +1,32 @@
 <template>
-  <header class="topo">
+  <header class="header">
     <nav class="navbar navbar-expand-lg navbar-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
+      <div class="container-fluid row">
+        <div class="col-2">
+          <router-link class="navbar-brand" to="/"><img id="logo" src="./imagens/logo.png" alt="Ícone do site"></router-link>
+        </div>
+        <div class="collapse navbar-collapse col-4 menu" id="navbarSupportedContent">
+                <router-link to="/vinhos" class="nav-link">Vinhos</router-link>
+
+                <router-link to="/" class="nav-link">Sobre</router-link>
+
+                <router-link to="/" class="nav-link">Contato</router-link>
+
+        </div>
+        <div class="collapse navbar-collapse col-5 campoPesquisa" id="navbarSupportedContent">
           <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <input class="form-control" type="search" placeholder="Busque por vinhos e outros usuários..." autocomplete="off" id="campoPesquisa" size="63" onfocus="this.value='';">
           </form>
         </div>
-      </div>
+        <div class=" collapse navbar-collapse col-1 menu" id="navbarSupportedContent">
+          <router-link to="/" class="nav-link">Fazer login</router-link>
+        </div>
+        <div>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div> 
     </nav>
   </header>
 </template>
@@ -46,14 +38,10 @@ export default {
 </script>
 
 <style>
-
-.topo .navbar {
+.header .navbar {
   background-color: #180566;
-}
-
-.topo {
-    background-color: #180566;
-    height: 100px;
+  display: flex;
+  
 }
 
 #logo{
@@ -91,12 +79,14 @@ textarea:focus, input:focus, select:focus{
   align-items: center;
   justify-content: space-around;
   font: 20px Georgia normal;
-  color: white;
-}
-.topo a:link, .topo a:visited, .topo a:hover, .topo a:active {
-  color: white;
-  text-decoration: none;
 }
 
+.header a:link, .header a:active, .header a:visited {
+  color: white;
+}
+
+.header a:hover {
+  color: rgb(173, 93, 173);
+}
 
 </style>
