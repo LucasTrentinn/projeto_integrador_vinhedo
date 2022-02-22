c374a80aa0c0cdba82398d41d49cb6018e526823
Lucas Trentin <lucas.mtrentin@hotmail.com>
2022-02-22 16:40:59 -0300

    Adicionando os arquivos pro Conteúdo 2 da Home

---
+++ Site/Site_Vinho/src/components/Conteudo2Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="seila">
+      Conteúdo 2
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.seila {
+    background-color: #4b0082 ;
+    height: 500px;
+}
+</style>
--- Site/Site_Vinho/src/views/HomeView.vue
+++ Site/Site_Vinho/src/views/HomeView.vue
@@ -1,10 +1,16 @@
 <template>
-<div>aaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+<div>
+    <div>Conteúdo1</div>
+    <conteudo-2-home />
+    <div>Conteúdo3</div>
+</div>
 
 </template>
 
 <script>
+import Conteudo2Home from '../components/Conteudo2Home.vue'
 export default {
+  components: { Conteudo2Home },
 
 }
 </script>
