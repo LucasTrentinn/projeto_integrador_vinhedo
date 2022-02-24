054adf5466abbbee85f21255a15fe8df1ba8343a
Lucas Trentin <lucas.mtrentin@hotmail.com>
2022-02-24 11:31:02 -0300

    Adição de testes no componente ItemVinho.vue

--- Site/Site_Vinho/src/components/ItemVinho.vue
+++ Site/Site_Vinho/src/components/ItemVinho.vue
@@ -1,10 +1,26 @@
 <template>
-  
+  <div class="item-vinho">
+      <router-link to="/vinhos/id">
+      <h1>{{ vinhos }}</h1>
+      <div class="item" v-for="vinho in vinhos" :key="vinho.id">
+          <img src="https://via.placeholder.com/440x470" alt="...">
+      </div>
+      </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            vinhos: [],
+        }
+    },
+    mounted() {
+        fetch("https://vinhos-dad4.restdb.io/rest/vinhos?apikey=d815307d94c90d5acdf9777002277880e7b3d")  
+        .then(response => response.json())
+        .then(data => this.vinhos = data);
+    }
 }
 </script>
 
