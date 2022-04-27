c0fc737a9f582b5ac9d54550971c884856e6a1f6
Lucas Trentin <lucas.mtrentin@hotmail.com>
2022-04-27 14:39:29 -0300

    Att - Porta de acesso para 3001

--- Extras/api/src/server.js
+++ Extras/api/src/server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { join } = require('path')
-const port = 3000
+const port = 3001
 const Modelo = require('./framework/Modelo')
 
 
