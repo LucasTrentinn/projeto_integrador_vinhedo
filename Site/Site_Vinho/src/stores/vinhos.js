9d75e6ef7ded92f2c7a7b9c3330ad89c83472e9b
Lucas Trentin <lucas.mtrentin@hotmail.com>
2022-05-02 16:32:53 -0300

    Integração da API na VinhoView e VinhosView

--- Site/Site_Vinho/src/stores/vinhos.js
+++ Site/Site_Vinho/src/stores/vinhos.js
@@ -4,15 +4,23 @@ import { api } from "../api";
 export const useVinhosStore = defineStore({
   id: 'vinhos',
   state: () => ({
-    vinhos: []
+    vinhos: [],
+    vinho: []
   }),
   getters: {
-
   },
   actions: {
     async consultarTodos() {
       const res = await api.get('/vinhos')
       this.vinhos = res.data
+    },
+    async consultarVinho({}, id) {
+      return api.get(`/vinhos/${id}`)
+    },
+    async selecionarVinho(id) {
+      const res = await api.get(`/vinhos/${id}`)
+      this.vinho = res.data
+      console.log(this.vinho)
     }
   }
 })
