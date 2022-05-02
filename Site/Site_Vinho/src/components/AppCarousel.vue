f6be1bfcda25ed7dd8f925e7943eacce7a627f67
Victor Augusto de Almeida Silva <victoraugustoas41@gmail.com>
2022-05-02 15:08:44 -0300

    arrumar botão

--- Site/Site_Vinho/src/components/AppCarousel.vue
+++ Site/Site_Vinho/src/components/AppCarousel.vue
@@ -3,17 +3,17 @@
     <div class="carousel-inner">
       <div :class="{ 'carousel-item': true, active: index == 0 }" v-for="(item, index) in dados" :key="index">
         <div class="row">
-          <slot :item="i" v-for="(i, index) in item" :key="index" />
+          <slot class="items" :item="i" v-for="(i, index) in item" :key="index" />
         </div>
       </div>
     </div>
-    <button class="carousel-control-prev btn-style" type="button" :data-bs-target="`#${carouselName}`"
+    <button class="carousel-control-prev carousel- btn-style btn" type="button" :data-bs-target="`#${carouselName}`"
       data-bs-slide="prev" :class="{'carousel-menor': carouselMenor}">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
 
-    <button class="carousel-control-next btn-style" type="button" :data-bs-target="`#${carouselName}`"
+    <button class="carousel-control-next btn-style btn" type="button" :data-bs-target="`#${carouselName}`"
       data-bs-slide="next" :class="{'carousel-menor': carouselMenor}">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
@@ -61,14 +61,29 @@ export default {
 </script>
 
 <style>
+.items {
+  display: flex;
+  justify-content: center;
+}
+
 .btn-style {
   height: 60px;
   width: 60px;
-  position: absolute;
   top: 250px;
+  border: 2px solid black; 
+  border-radius: 100%;
+  background-color: black;
+  opacity: 30%;
+  
 }
 
 .carousel-menor {
   top: 100px ;
+  
+  
 }
+/* .carousel-maior{
+  left: 11px;
+} */
+
 </style>
